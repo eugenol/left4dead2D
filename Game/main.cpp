3b6af391189043ca870fcb282412124ed9d346c2
eugenol <eug@live.co.za>
2015-04-14 18:43:33 +0200

    Start game loop

--- Game/main.cpp
+++ Game/main.cpp
@@ -2,23 +2,35 @@
 #include <allegro5/allegro_native_dialog.h>
 
 int main(int argc, char **argv)
-
 {
-	ALLEGRO_DISPLAY *display = NULL;
+	//Display width & height, can move to a header file later.
+	const int DISPLAY_HEIGHT = 600;
+	const int DISPLAY_WIDTH = 800;
 
+	bool game_done = false; // used for game loop
+	bool redraw = false; // used for rendering
+
+	ALLEGRO_DISPLAY *display = NULL; //Pointer to display.
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL; //Pointer to event queue
+	ALLEGRO_TIMER *timer = NULL; //Pointer to timer
+
+	//Initialise allegro, if unsuccesful, show error.
 	if (!al_init())
 	{
 		al_show_native_message_box(al_get_current_display(), "Error", "Error", "Could not initatialize allegro",
 			NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		return -1;
+		return -1; //exit program
 	}
 
-	display = al_create_display(640, 480);
+	// create display & check if succesful.
+	display = al_create_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
 	if (!display) {
 		al_show_native_message_box(al_get_current_display(), "Error", "Error", "Could not initatialize the display",
 			NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		return -1;
+		return -1; // exit program
 	}
+
+	al_set_window_title(display, "Zombie Rush");
 
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 
