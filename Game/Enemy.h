dec828acc326d9e99c0e2e5f1b3f02e2bdcf269e
TheDarkFlame <david.theo.parker@gmail.com>
2015-04-30 23:54:49 +0200

    add declarations for enemy class functions (using primitives)

--- Game/Enemy.h
+++ Game/Enemy.h
@@ -8,15 +8,21 @@
 
 class Enemy :public GameEntity{
 private:
+	int rad_x = 15;
+	int rad_y = 15;
 	int type;
 	int hitpoints;//hitpoints set by difficulty
 	int armortype;//to allow resistances to certain weapons
 	int regenRate;//rate is set by difficulty
 public:
-	Enemy(int type, int pos_x, int pos_y, int hitpoints, int armortype, int regenRate);
+	Enemy(int ID,int type, int pos_x, int pos_y,int speed_x,int speed_y,int direction_x,int direction_y,
+		int hitboxRadius, ALLEGRO_BITMAP *image, bool active, int hitpoints, int armorType, int regenRate);
+	~Enemy();
 	void chasePlayer();
+	void draw();
+	void update();
 };
 
 #endif
-/*GameEntity(int pos_x, int pos_y, int speed_x, int speed_y, int direction_y, int direction_x, bool active,
-int hitboxRadius, int ID, ALLEGRO_BITMAP *image);*/
+/*GameEntity(int pos_x, int pos_y, int speed_x, int speed_y, int direction_x,
+int direction_y, bool active,int hitboxRadius, int ID, ALLEGRO_BITMAP *image);*/
