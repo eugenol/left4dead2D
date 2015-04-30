56c7f1c5b2dea8b79a87ce98645e67267c35b29d
eugenol <eug@live.co.za>
2015-04-30 19:21:35 +0200

    Add mouse input to input manager

--- Game/InputManager.cpp
+++ Game/InputManager.cpp
@@ -5,6 +5,12 @@ InputManager::InputManager()
 {
 	for (int i = 0; i < numKeys; i++)
 		keys.push_back(false);
+
+	for (int i = 0; i < 3; i++)
+		mouseButton[i] = false;
+
+	mouse_x = 0;
+	mouse_y = 0;
 }
 
 
@@ -69,9 +75,36 @@ void InputManager::getInput(ALLEGRO_EVENT &ev)
 			break;
 		}
 	}
+
+	// In case of mouse movement or button press, update mouse state
+	if (ev.type == ALLEGRO_EVENT_MOUSE_AXES || ev.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY
+		|| ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN || ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
+	{
+		al_get_mouse_state(&mouseState);
+		mouse_x = mouseState.x;
+		mouse_y = mouseState.y;
+		mouseButton[LEFTM] = mouseState.buttons & 1; // left mouse button pressed
+		mouseButton[RIGHTM] = mouseState.buttons & 2; // right mouse button pressed
+		mouseButton[MIDDLEM] = mouseState.buttons & 3; // middle mouse button pressed
+	}
 }
 
 bool InputManager::isKeyPressed(int key)
 {
 	return keys[key];
+}
+
+bool InputManager::isMouseButtonPressed(int button)
+{
+	return mouseButton[button];
+}
+
+float InputManager::getMouseX()
+{
+	return mouse_x;
+}
+
+float InputManager::getMouseY()
+{
+	return mouse_y;
 }
