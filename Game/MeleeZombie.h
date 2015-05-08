6a435336d16936f844fbcc0289e59362bc8b3c56
TheDarkFlame <david.theo.parker@gmail.com>
2015-05-08 17:51:19 +0200

    added zombie movement, also added a pixel at the centre of each entity's position for debugging

--- Game/MeleeZombie.h
+++ Game/MeleeZombie.h
@@ -7,11 +7,8 @@
 class MeleeZombie :public Enemy {
 private:
 	ALLEGRO_BITMAP * image;
-	int defaultHitpoints = 87;
-	int defaultRegen = 0;
-	int defaultSpeed = 4;//speed is broken into x and y parts
 	void UpdateDirection();
-	void setDirection(int & direction, int pos_x, int pos_y, int dest_x, int dest_y);//sets the animation direction correctly
+	void setDirection(int & direction, float angle);//sets the animation direction towards zombie destination
 public:
 	void update();
 	//quick constructor (presets most values)
